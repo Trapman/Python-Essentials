0c5283273bff0da16a55f8c8ea6af0405d29c0c9
Trapman <david.a.shuttleworth@gmail.com>
2020-02-19 16:23:30 -0500

    Create types and values.py

---
+++ types and values.py
@@ -0,0 +1,29 @@
+# STRING TYPES ############################
+# use quote marks ' ' OR " "
+x = 'seven'
+print(type(x))  # returns string 
+
+# you can run methods on strings since they're object. Here are some common ones:
+.capitalize()
+.upper()
+.lower()
+
+# f string
+a = 8
+b = 9
+x = f'seven {a} {b}'
+print(x)                   # returns x is seven 8 9
+
+
+# NUMERIC TYPES ############################
+# int()
+x = 7
+
+
+# float()
+
+# BOOLEAN TYPES ##############################
+
+# SEQUENCE TYPES ##################################
+
+# type() and id() ###########################3
