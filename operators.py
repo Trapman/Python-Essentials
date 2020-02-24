022e89c98dc2c64cc1e74f0ad00e1806cf710326
Trapman <david.a.shuttleworth@gmail.com>
2020-02-24 14:54:00 -0500

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,44 @@
+# CONDITIONAL OPERATORS######################
+# equal ==
+# not equal !=
+# <, >, <=, >=
+
+# LOGICAL OPERATORS ################
+# and (e.g. x and y), or , not (e.g. not x)
+
+# IDENTITY OPERATOR ###########
+x is y # True if they're the same object
+x is not y
+
+# MEMBERSHIP OPERATOR ################
+x in y # True if x member in collection of y
+x not in y
+
+# CONDITIONAL ASSIGNMENT #########
+# called a turnary operator. you need if and else
+hungry = True
+x = 'Feed Buford now!' if hungry else 'Do NOT feed Buford.'
+print(x)
+
+# ARITHMETIC OPERATORS #####################
+x = 3
+y = 7
+z = x + y
+print(z)
+
+# BITWISE OPERATORS ################
+# & and
+# | or
+# ^ Xor
+# << shift left
+# >> shift right
+
+# COMPARISON OPERATORS ###########
+# < > <= >= == !=
+x = 100
+y = 200
+if x < y:
+    print('comparison is true')
+else:
+    print('comparison is false')
+
