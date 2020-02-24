d241c8d6f87c26b3929c5ef0ec5da42803d03216
Trapman <david.a.shuttleworth@gmail.com>
2020-02-24 13:03:07 -0500

    Update types and values.py

--- types and values.py
+++ types and values.py
@@ -16,14 +16,72 @@ print(x)                   # returns x is seven 8 9
 
 
 # NUMERIC TYPES ############################
-# int()
+# INTEGERS ################################################
+int()
 x = 7
 
 
-# float()
+# FLOATING #################################################
+  
+# when dealing with money, use this:
+  from decimal import *
+  
+#BOOLEAN TYPE ########################################
+#True/Falso
 
-# BOOLEAN TYPES ##############################
+# SEQUENCE TYPES ###########################################################
+# list = [] #############################################################
+x = [1,2,3,4,5]
+for i in x:
+    print(i)
+# reassigning and indexing: use []
+x[3] = 99  # reassigns item[3] to 99
 
-# SEQUENCE TYPES ##################################
+# tuple = () ###############################################################
+# you can't reassign a tuple, they're immutable
+x = (1,2,3,4,5)
+for i in x:
+    print(x)
 
-# type() and id() ###########################3
+# also, you can create a sequence using range()
+# syntax: range(start, stop, step)
+# also immutable
+x = range(5)
+for i in x:
+    print(x)
+    
+x = range(5, 50, 5) 
+for i in x:
+    print(i)
+    
+#to make it mutable, add in list()
+x = list(range(5))
+x[2] = 42
+for i in x:
+    print(i)
+    
+# dictionary x = {key : value} ####################################################################
+x = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five' :5}
+for k, v in x.items():
+    print('k: {}, v: {}'.format(k, v)) #he uses this .format() method a lot
+  
+# dicts are mutable
+x = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five' :5}
+x['three'] = 42   # you can change value like this
+for k, v in x.items():
+    print('k: {}, v: {}'.format(k, v))
+    
+# TYPE() and ID() ###################################################################################
+x = (1,'two',3.0,[4, 'four'],5) 
+print(x)   
+print(type(x[2]))
+print(id(x[2]))
+
+# how to check if something is a list/tuple/etc.
+# use isinstance()
+if isinstance(x, tuple):
+    print('yes it is a tuple')
+elif isinstance(x, list):
+    print('wait, actually it is a list')
+else:
+    print('nope!')
